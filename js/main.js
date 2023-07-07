4cec76c75e797f356311533c962e33531a110e45
sayedhany <sayedh@mail.ru>
2023-07-07 16:15:11 +0300

    add

--- js/main.js
+++ js/main.js
@@ -6,4 +6,15 @@ $(function () {
     navH = $(".navbar").innerHeight();
   console.log(winH, upperH, navH);
   $(".slider , .carousel-item").height(winH - (upperH + navH));
+  $(".featured-work ul li").on("click", function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    if ($(this).data("class") === "all") {
+      $(".shuffle-imgs .col-sm").css("opacity", 1);
+      console.log(this);
+    } else {
+      console.log(this);
+      $(".shuffle-imgs .col-sm").css("opacity", "0.08");
+      $($(this).data("class")).parent().css("opacity", 1);
+    }
+  });
 });
