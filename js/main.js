c58ceef63ac7b9a59d1894816b60db5fdfb09b3f
sayedhany <sayedh@mail.ru>
2023-07-09 18:41:32 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 $(function () {
   "use strict";
-
-  let winH = $(window).height(),
+  // Adjust Slider Height
+  var winH = $(window).height(),
     upperH = $(".upper-bar").innerHeight(),
     navH = $(".navbar").innerHeight();
-  console.log(winH, upperH, navH);
-  $("#main-nav > ul > li").on("click", function () {
-    $(this).addClass("active").siblings().removeClass("active");
-  });
-  $(".slider , .carousel-item").height(winH - (upperH + navH));
+  $(".slider, .carousel-item").height(winH - (upperH + navH));
+
+  // Featured Work Shuffle
   $(".featured-work ul li").on("click", function () {
     $(this).addClass("active").siblings().removeClass("active");
     if ($(this).data("class") === "all") {
-      $(".shuffle-imgs .col-sm").css("opacity", 1);
-      console.log(this);
+      $(".shuffle-imgs .col-md").css("opacity", 1);
     } else {
-      console.log(this);
-      $(".shuffle-imgs .col-sm").css("opacity", "0.08");
+      $(".shuffle-imgs .col-md").css("opacity", ".08");
       $($(this).data("class")).parent().css("opacity", 1);
     }
+  });
+  $("#main-nav > ul > li").on("click", function () {
+    $(this).addClass("active").siblings().removeClass("active");
   });
 });
